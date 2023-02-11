8ca4184bc5bef487861b6c178e90f934e4a25349
Shen Z <shenAwesome@gmail.com>
2023-02-11 22:51:49 +1100

    gogo

--- desktop/Program.cs
+++ desktop/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using XDMessaging;
 
 namespace SMarkdownReader {
     internal static class Program {
+        private static Mutex mutex = null;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -32,7 +29,7 @@ namespace SMarkdownReader {
             }
 
             string appName = Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location);
-            var mutex = new Mutex(true, appName, out bool firstInstance);
+            mutex = new Mutex(true, appName, out bool firstInstance);
             XDMessagingClient client = new XDMessagingClient();
             var channel = appName + "+commands";
             if (firstInstance) {
